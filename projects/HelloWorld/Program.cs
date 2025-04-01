44316c0044b09a8efb09d03c937d5d4db6f3b4f5
NiteRynYaUwU <zhu11yu23@gmail.com>
2025-04-01 20:38:32 +1100

    first commit

---
+++ projects/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 100;
+        int m = 200;
+        int sum = 0;
+        sum = n + m;
+
+
+        Console.WriteLine(sum);
+        Console.ReadKey();
+
+    }
+}
